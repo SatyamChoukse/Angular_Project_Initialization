b23c31a8e676ab8b28ceb0944ff6b292d57380ac
SatyamChoukse <satychouksey07@gmail.com>
2024-02-27 17:30:13 +0530

    added auth guard of user and admin

---
+++ src/app/guards/user.guard.ts
@@ -0,0 +1,13 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { roleType } from '../enums/roleType.enum';
+import { inject } from '@angular/core';
+
+export const userGuard: CanActivateFn = (route, state) => {
+  let roletype = localStorage.getItem('roleType');
+  if (roletype) {
+    if (roletype == roleType.admin.toString()) {
+      return true;
+    }
+  }
+  return inject(Router).navigate(['/home']);
+};
