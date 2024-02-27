640e58e5900908a7646450b362b7673e3f9a00c1
SatyamChoukse <satychouksey07@gmail.com>
2024-02-27 15:02:49 +0530

    changes in scss with or without login

--- src/app/public/header/header.component.ts
+++ src/app/public/header/header.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit,OnChanges{
+  
+  public token:boolean=false;
+  
+  ngOnChanges(): void {
+    if(localStorage.getItem('jwtToken')){
+      this.token=!this.token;
+    }
+  }
 
+  ngOnInit(): void {
+    if(localStorage.getItem('jwtToken')){
+      this.token=!this.token;
+    }
+  }
+
+  public logout(){
+    localStorage.clear();
+    this.token=!this.token;
+  }
+  
 }
